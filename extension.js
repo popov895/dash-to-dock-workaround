c810ed368b2a74f92b34985dfbe546e59de03bbb
Eugene Popov <popov895@ukr.net>
2024-01-25 00:40:34 +0200

    Better workaround

--- extension.js
+++ extension.js
@@ -4,33 +4,51 @@ const { Gio, GLib } = imports.gi;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 
-let showDashToDockTimeoutId;
-
-var enable = () => {
-    const dashToDockSettingsSchema = Gio.SettingsSchemaSource.get_default().lookup(
-        `org.gnome.shell.extensions.dash-to-dock`,
-        true
-    );
-    if (!dashToDockSettingsSchema) {
-        return;
-    }
-    const dashToDockSettings = new Gio.Settings({
-        settings_schema: dashToDockSettingsSchema,
-    });
-    dashToDockSettings.set_boolean(`dock-fixed`, false);
-    showDashToDockTimeoutId = GLib.timeout_add(
-        GLib.PRIORITY_DEFAULT,
-        ExtensionUtils.getSettings().get_int(`delay`),
-        () => {
-            dashToDockSettings.set_boolean(`dock-fixed`, true);
-            return GLib.SOURCE_REMOVE;
+class Extension {
+    enable() {
+        const dashToDockSettingsSchema = Gio.SettingsSchemaSource.get_default().lookup(
+            `org.gnome.shell.extensions.dash-to-dock`,
+            true
+        );
+        if (!dashToDockSettingsSchema) {
+            return;
         }
-    );
-};
 
-var disable = () => {
-    if (showDashToDockTimeoutId) {
-        GLib.Source.remove(showDashToDockTimeoutId);
-        showDashToDockTimeoutId = null;
+        const values = {
+            'animation-time': GLib.Variant.new_double(0),
+            'dock-fixed': GLib.Variant.new_boolean(false),
+        };
+
+        const dashToDockSettings = new Gio.Settings({
+            settings_schema: dashToDockSettingsSchema,
+        });
+
+        const snapshot = {};
+        for (const key in values) {
+            snapshot[key] = dashToDockSettings.get_value(key);
+            dashToDockSettings.set_value(key, values[key]);
+        }
+
+        this._showDashToDockTimeoutId = GLib.timeout_add(
+            GLib.PRIORITY_DEFAULT,
+            ExtensionUtils.getSettings().get_int(`delay`),
+            () => {
+                for (const key in snapshot) {
+                    dashToDockSettings.set_value(key, snapshot[key]);
+                }
+                return GLib.SOURCE_REMOVE;
+            }
+        );
     }
-};
+
+    disable() {
+        if (this._showDashToDockTimeoutId) {
+            GLib.Source.remove(this._showDashToDockTimeoutId);
+            delete this._showDashToDockTimeoutId;
+        }
+    }
+}
+
+function init() {
+    return new Extension();
+}
